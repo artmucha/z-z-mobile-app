e08f7da925b00b2421a9b7d14083a5e46a63cb63
Artur Mucha <arti.mucha@gmail.com>
2021-10-10 17:06:54 +0200

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'nunito-light': require('./assets/fonts/Nunito-Light.ttf'),
+    'nunito': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  })
+};
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if(!dataLoaded) return (
+    <AppLoading 
+      // @ts-ignore
+      startAsync={fetchFonts} 
+      onFinish={() => setDataLoaded(true)}
+      onError={() => console.log('error')}
+    />
+  )
+
   return (
     <View style={styles.container}>
       <Text>Start</Text>
